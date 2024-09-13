dd4b2f112e0d6a68fb79b72f7b0e0a73d34f6fa5
Alvien01 <alviantampan0@gmail.com>
2024-09-13 23:45:04 +0700

    Data Uji+Data Training

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [DataTrainingController::class, 'index']);
+
+Route::get('/DataTrainingCuaca', [DataTrainingController::class, 'index']);
+Route::post('/DataTrainingCuaca/validation_form', [DataTrainingController::class, 'validation_form']);
+Route::get('/DataTrainingCuaca/hapus/{id}', [DataTrainingController::class, 'hapus']);
+Route::post('/DataTrainingCuaca/ubah/{id}', [DataTrainingController::class, 'ubah']);
+
+Route::get('/DataUjiCuaca', [DataUjiController::class, 'index'])->name('data-uji-cuaca.index');
+Route::post('/DataUjiCuaca/validation_form', [DataUjiController::class, 'validation_form'])->name('data-uji-cuaca.hitung');
+Route::get('/DataUjiCuaca/hapus/{id}', [DataUjiController::class, 'hapus'])->name('data-uji-cuaca.hapus');
+Route::post('/DataUjiCuaca/ubah/{id}', [DataUjiController::class, 'ubah'])->name('data-uji-cuaca.ubah');
+
